a9177836664351bffd8b53c5acadd06f3c59decb
Vikas Singh <itvika@gmail.com>
2024-08-22 22:56:17 +0530

    simple interest program

---
+++ basic/SimpleInterest.java
@@ -0,0 +1,9 @@
+class SimpleInterest{
+    public static void main(String[] args){
+        int p=1000;
+        int r=12;
+        int t=3;
+        float si=(p*r*t)/100;
+        System.out.println("Simple interest="+si);
+    }
+}
